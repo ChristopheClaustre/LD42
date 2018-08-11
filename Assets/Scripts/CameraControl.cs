c91ad8af338302394d78ed6c0daa02b3d4b96a5a
Xavier Chalut <xavier.chalut@gmail.com>
2018-08-11 12:38:45 +0200

    [FCT] Ajout du script de contrôle de camera

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,107 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class CameraControl : MonoBehaviour {
+
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+    
+    [Range(0.0f,100.0f)]
+    public float m_mainSpeed = 10.0f; //regular speed
+    [Range(0.0f, 1000.0f)]
+    public float m_camSens = 100f; //How sensitive it with mouse
+    [Range(0.0f, 1.0f)]
+    public float m_radius = 0.05f;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        mousePos.x -= 0.5f;
+        mousePos.y -= 0.5f;
+
+        // No effect circle zone 
+        if ( (mousePos.x*mousePos.x) + (mousePos.y * mousePos.y) < (m_radius * m_radius))
+        {
+            mousePos.x = 0;
+            mousePos.y = 0;
+        }
+
+        //Pitch 
+        Vector3 pitch = new Vector3(-mousePos.normalized.y,0,0);
+        transform.Rotate(pitch * Time.deltaTime * m_camSens * Mathf.InverseLerp(0, 0.5f, Mathf.Abs(mousePos.y)));
+        //Yaw
+        Vector3 yaw = new Vector3(0, mousePos.normalized.x, 0);
+        transform.Rotate(yaw * Time.deltaTime * m_camSens * Mathf.InverseLerp(0, 0.5f, Mathf.Abs(mousePos.x)), Space.World);
+
+        //Roll
+        if (Input.GetAxis("Roll") < 0)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * m_camSens);
+        }
+        if (Input.GetAxis("Roll") > 0)
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * m_camSens);
+        }
+
+        //Keyboard commands
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        p = p * m_mainSpeed;
+        p = p * Time.deltaTime;
+        transform.Translate(p);
+
+    }
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+    private Vector3 GetBaseInput()
+    {
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetButton("Forward"))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetButton("Backward"))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetButton("Left"))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetButton("Right"))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        if (Input.GetButton("Up"))
+        {
+            p_Velocity += new Vector3(0, 1, 0);
+        }
+        if (Input.GetButton("Down"))
+        {
+            p_Velocity += new Vector3(0, -1, 0);
+        }
+        return p_Velocity;
+    }
+    #endregion
+}
