bf8a941d8d9fbe791beca78ce4977da43f85de95
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-08-11 13:10:12 +0200

    [FCT] ajout du GameManager (ne fait presque rien pour le moment)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,11 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 
 /***************************************************/
 /***  THE CLASS             ************************/
 /***************************************************/
-public class SettingsManager :
+public class GameManager :
     MonoBehaviour
 {
     #region Sub-classes/enum
@@ -17,16 +16,11 @@ public class SettingsManager :
     /***  SUB-CLASSES/ENUM      ************************/
     /***************************************************/
 
-    [System.Serializable]
-    public class Settings
-    {
-        public float m_rayonSphere;
-        public int m_initialPlateformCount;
-        public float m_minimalDistanceBetweenPlateform;
-        public float m_roundDuration;
-        public float m_playerSpeed;
-        public float m_playerAngularSpeed;
-    }
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
 
     #endregion
     #region Property
@@ -42,13 +36,7 @@ public class SettingsManager :
     /***  CONSTANTS             ************************/
     /***************************************************/
 
-    public static Settings Inst
-    {
-        get
-        {
-            return m_settings;
-        }
-    }
+
 
     #endregion
     #region Attributes
@@ -56,12 +44,9 @@ public class SettingsManager :
     /***  ATTRIBUTES            ************************/
     /***************************************************/
 
-
-    private static Settings m_settings = null;
-
-    [SerializeField]
-    private TextAsset m_settingsFile;
-
+    private int m_round = -1;
+    private float m_nextRoundIn = 0;
+    
     #endregion
     #region Methods
     /***************************************************/
@@ -73,15 +58,16 @@ public class SettingsManager :
     // Use this for initialization
     private void Start()
     {
-        Debug.Assert(m_settingsFile != null, "Settings file not found...");
-
-        m_settings = JsonUtility.FromJson<Settings>(m_settingsFile.text);
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
+        m_nextRoundIn -= Time.deltaTime;
 
+        if (m_nextRoundIn <= 0)
+            NextRound();
     }
 
     /********  OUR MESSAGES     ************************/
@@ -91,6 +77,14 @@ public class SettingsManager :
     /********  PROTECTED        ************************/
 
     /********  PRIVATE          ************************/
+
+    private void NextRound()
+    {
+        ++m_round;
+        Debug.Log("Beginning of round " + (m_round + 1));
+
+        m_nextRoundIn = SettingsManager.Inst.m_roundDuration;
+    }
 
     #endregion
 }
