379cf751defa8f7b208fac7f06311f6e1ebdd878
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-08-11 18:59:29 +0200

    [FCT] script de gestion d'une bar (de progression)

---
+++ Assets/Scripts/ManageBar.cs
@@ -0,0 +1,86 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class ManageBar :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    [SerializeField]
+    private Transform m_bar = null;
+    [SerializeField]
+    private Transform m_canvas = null;
+
+    public float m_value;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Assert(m_bar, "Why there isn't any healthbar on this enemy ?");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!m_bar || !m_canvas) return;
+
+        m_bar.localScale = new Vector3(m_value, 1, 1);
+        m_canvas.LookAt(Camera.main.transform);
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
