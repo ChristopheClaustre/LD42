40cfcb06736fa97f5fb7ba37e03ec37ce533709e
ChristopheClaustre <christophe.claustre.31@gmail.com>
2018-08-13 18:25:31 +0200

    [DATA] mise à jour des canvas des tourelles + ajout d'une représentation du niveau

---
+++ Assets/Scripts/UI/ShowLevels.cs
@@ -0,0 +1,77 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class ShowLevels :
+    MonoBehaviour
+{
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    public int Value
+    {
+        get
+        {
+            return m_value;
+        }
+
+        set
+        {
+            m_value = value;
+            for (int i = 0; i < m_stars.Count; i++)
+            {
+                m_stars[i].SetActive(i < m_value);
+            }
+        }
+    }
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    [SerializeField]
+    protected List<GameObject> m_stars;
+
+    [SerializeField]
+    private int m_value = 0;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        Value = m_value;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
